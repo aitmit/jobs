0fb654336d4ef020c7b7f386ad3cb84675c84ca0
AIT <ait.mit@gmail.com>
2010-12-25 02:56:04 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobs::Application.config.secret_token = 'dd07037666ba1762f31e9a6c8ff1bfb786704ec3ea46b2b87ccb859a73e165e3596424b8ac2b3bbde4e27f7155f4a57509abc99b935f565320ea47810aa8e78b'
